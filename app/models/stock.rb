7013783374c76f0d46a78eec5bdb63c352c53763
ddd88 <dalewalk10@gmail.com>
2018-04-29 19:45:12 +1000

    Add search action aand fix errors

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,5 @@
 class Stock < ApplicationRecord
-	has_many :user_stocka
+	has_many :user_stocks
 	has_many :users, through: :user_stocks
 
 	def self.find_by_ticker(ticker_symbol)
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   devise_for :views
 
-  resources :user_stocks, except: [:show, :edit, :update]
-
   root 'welcome#index'
-
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
+  resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
   get 'my_friends', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
