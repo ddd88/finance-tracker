9ca8a6f3fb459d200311e3e9b174d7c90624eaa3
ddd88 <dalewalk10@gmail.com>
2018-04-29 20:10:29 +1000

    Add and Remove friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+
+    if current_user.save
+      flash[:success] = "Friend was successfully added"
+    else
+      flash[:danger] = "There was something wrong with the friend request"
+    end
+    redirect_to my_friends_path
+  end
 
 	def show
 		@user = User.find(params[:id])
